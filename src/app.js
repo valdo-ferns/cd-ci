8da3cbfffac2e26e48f8d5df48887f89378da56f
valdo-ferns <valdo@cidroy.com>
2025-02-24 18:07:54 +0530

    test api 2

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.get("/test", (req, res) => {
 // Error Handler
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
